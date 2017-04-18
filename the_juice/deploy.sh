89762f03e48e162bb601950577a9a863523f2904
Justin Leong <justinleong16@gmail.com>
2017-04-17 23:53:55 -0700

    Add deploy script

---
+++ the_juice/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Initial setup
+mix deps.get --only prod
+MIX_ENV=prod mix compile
+
+# Compile assets
+brunch build --production
+MIX_ENV=prod mix phoenix.digest
+
+# Custom tasks (like DB migrations)
+MIX_ENV=prod mix ecto.create
+MIX_ENV=prod mix ecto.migrate
+
+# Finally run the server
+PORT=4001 MIX_ENV=prod mix phoenix.server
