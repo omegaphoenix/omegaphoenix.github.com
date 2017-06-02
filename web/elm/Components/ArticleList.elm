06e84ff964cb9eb6fa69c31f9c80629074d36e8c
Justin Leong <justinleong16@gmail.com>
2017-06-02 02:23:56 -0700

    Add Fetch Articles button

--- web/elm/Components/ArticleList.elm
+++ web/elm/Components/ArticleList.elm
@@ -1,27 +1,48 @@
-module Components.ArticleList exposing (view)
+module Components.ArticleList exposing (..)
 
-import Html exposing (Html, text, ul, li, div, h2)
+import Html exposing (Html, text, ul, li, div, h2, button)
 import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
 import List
 import Article
 
-articles : List Article.Model
+type alias Model =
+  { articles: List Article.Model }
+
+articles : Model
 articles =
-  [ { title = "Article 1", url = "http://google.com", postedBy = "justin", postedOn = "07/02/17" }
-  , { title = "Article 2", url = "http://google.com", postedBy = "justin", postedOn = "07/02/17" }
-  , { title = "Article 3", url = "http://google.com", postedBy = "justin", postedOn = "07/02/17" }
-  ]
+  { articles =
+    [ { title = "Article 1", url = "http://google.com", postedBy = "justin", postedOn = "07/02/17" }
+    , { title = "Article 2", url = "http://google.com", postedBy = "justin", postedOn = "07/02/17" }
+    , { title = "Article 3", url = "http://google.com", postedBy = "justin", postedOn = "07/02/17" } ] }
+
+type Msg
+  = NoOp
+  | Fetch
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    NoOp ->
+      (model, Cmd.none)
+    Fetch ->
+      (articles, Cmd.none)
+
+initialModel : Model
+initialModel =
+  { articles = [] }
 
 renderArticle : Article.Model -> Html a
 renderArticle article =
   li [ ] [ Article.view article ]
 
-renderArticles : List (Html a)
-renderArticles =
-  List.map renderArticle articles
+renderArticles : Model -> List (Html a)
+renderArticles model =
+  List.map renderArticle model.articles
 
-view : Html a
-view =
+view : Model -> Html Msg
+view model =
   div [ class "article-list" ]
-  [ h2 [] [ text "Article List" ]
-  , ul [] renderArticles ]
+    [ h2 [] [ text "Article List" ]
+    , button [ onClick Fetch, class "btn btn-primary" ] [ text "Fetch Articles" ]
+    , ul [] (renderArticles model) ]
--- web/elm/TheJuice.elm
+++ web/elm/TheJuice.elm
@@ -5,6 +5,49 @@ import Html.Attributes exposing (class)
 
 import Components.ArticleList as ArticleList
 
-main : Html a
+-- MODEL
+
+type alias Model =
+  { articleListModel: ArticleList.Model }
+
+initialModel : Model
+initialModel =
+  { articleListModel = ArticleList.initialModel }
+
+init : (Model, Cmd Msg)
+init =
+  ( initialModel, Cmd.none )
+
+-- UPDATE
+
+type Msg
+  = ArticleListMsg ArticleList.Msg
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    ArticleListMsg articleMsg ->
+      let (updatedModel, cmd) = ArticleList.update articleMsg model.articleListModel
+      in ( { model | articleListModel = updatedModel }, Cmd.map ArticleListMsg cmd)
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div [ class "elm-app" ]
+    [ Html.map ArticleListMsg (ArticleList.view model.articleListModel) ]
+
+-- MAIN
+
+main : Program Never Model Msg
 main =
-  div [ class "elm-app" ] [ ArticleList.view ]
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
